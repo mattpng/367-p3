d59f7110323fee7e8645941619d172aded4ebb6c
mattpng <mpng@wisc.edu>
2017-11-05 21:11:30 -0600

    Update MinPQ.java

--- MinPQ.java
+++ MinPQ.java
@@ -1,16 +1,16 @@
+
+
 import java.util.Arrays;
 
 /**
- * GENERAL DIRECTIONS -
+ * MinPQ represents the priority queue where the minimum value is the highest
+ * priority. This priority queue is implemented using an array-based heap and
+ * stores the values greater than the median.
  *
- * 1. You may add private data fields and private methods as you see fit.
- * 2. Implement ALL the methods defined in the PriorityQueueADT interface.
- * 3. DO NOT change the name of the methods defined in the PriorityQueueADT interface.
- * 4. DO NOT edit the PriorityQueueADT interface.
- * 5. DO NOT implement a shadow array.
+ * <p>Bugs: NA
+ *
+ * @author Matt P'ng
  */
-
-
 
 public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
 {
@@ -18,12 +18,8 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
     private static final int INITIAL_SIZE = 10;
     private int numItems = 0;
 
-    //ADD MORE DATA PRIVATE DATA FIELDS AS YOU NEED.
-
-    public MinPQ()
-    {
+    public MinPQ(){
         this.items = (E[]) new Comparable[INITIAL_SIZE];
-        // TO-DO: Complete the constructor for any private data fields that you add.
     }
     
     
@@ -49,14 +45,12 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
      * @param item the item to add
      * @throws IllegalArgumentException if item is null
      */
-     public void insert(E item)
-     {
+     public void insert(E item){
     	 boolean done = false;
     	 if (item == null ) 
     		 throw new IllegalArgumentException();
     	 
-    	 if (numItems + 1 == items.length)
-    	 {
+    	 if (numItems + 1 == items.length) {
     		items = Arrays.copyOf(items, items.length *2);
     	 }
     	 
@@ -65,41 +59,34 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
     	 int check = index/2;
  	
     	 while (!done){
-    		 
-	 		if (index == 1)
-	 		{
+	 		if (index == 1){
 	 			done = true;
 	 			break;
 	 		}
 	 		
-	 		if (item.compareTo(items[check]) < 0)
-	 		{
+	 		if (item.compareTo(items[check]) < 0){
 	 			items[index] = items[check];
 	 			items[check] = item;
 	 			index = check;
-	 			check = index/2;
-	     		
+	 			check = index/2;		
 	 		}else{
 	 			done = true;
 	 		}	
 	 	}
-	 	
 	 	numItems++;
-	 
      }
     /**
      * Returns the highest priority item in the priority queue.
      *
      * MinPriorityQueue => it will return the smallest valued element.
-     * MaxPriorityQueue => it will return the largest valued element.
      *
      * @return the highest priority item in the priority queue.
      * @throws EmptyQueueException if priority queue is empty.
      */
     public E getMax() throws EmptyQueueException{
-    	if (isEmpty())
+    	if (isEmpty()){
     		throw new EmptyQueueException();
-    	
+    	}
     	return items[1];
     }
 
@@ -109,29 +96,29 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
      * queue accordingly.
      *
      * MinPriorityQueue => it will return and remove the smallest valued element.
-     * MaxPriorityQueue => it will return and remove the largest valued element.
      *
      * @return the highest priority item in the priority queue.
      * @throws EmptyQueueException if priority queue is empty.
      */
     public E removeMax() throws EmptyQueueException{
     	if (isEmpty() == true){throw new EmptyQueueException();}
-    	E tmp = items[1];
+    	E tmp = items[1]; // tmp is removed maximum value that will be returned
     	E tmp2 = null;
     	boolean done = false; 
-    	items[1] = items[numItems];
+    	items[1] = items[numItems]; // Set root of tree equal to last item in PQ
     	
-    	int index=1;
-    	int swapIndex = 0;
+    	int index=1; // Index of position checked against its children, one being swapIndex
+    	int swapIndex = 0; // Index of position of a child we will swap with 
     	
     	while(!done){
-    		
+    		// If index or index*2 is greater than numItems, no more swaps are possible.
     		if (index*2 > numItems || index*2+1 > numItems){
     			done=true;
     			break;
     		}
-    		
-    		
+
+    		// If either items[index*2] or items[index*2+1] is less than tmp, swap with the lesser priority
+    		// of the two.
     		if (tmp.compareTo(items[index*2]) < 0 || tmp.compareTo(items[index*2+1]) < 0 ){
     			if (items[index*2].compareTo(items[index*2+1]) <= 0){
     				swapIndex = index*2;
@@ -139,23 +126,20 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
     			else{
     				swapIndex = index*2+1;
     			}
-    			
+
+    			// Perform swap
     			tmp2 = items[index];
     			items[index] = items[swapIndex];
     			items[swapIndex] = tmp2;
-    			index = swapIndex;
-    			
+    			index = swapIndex;	
     		}
     		else{
     			done=true;
     			break;
     		}
     	}
-    
-   
     	numItems--;
     	return tmp;
-
     }
 
     /**
@@ -166,11 +150,4 @@ public class MinPQ<E extends Comparable<E>> implements PriorityQueueADT<E>
     public int size(){
 		return numItems;
     }
-    
-    
-    // To be removed
-    public E get(int i){
-    	return items[i];
-    }
-    
 }
